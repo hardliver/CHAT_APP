5e087b5a26c7048aa8452d706c8472a4c3a819a6
hammerandnail <yelionel26@gmail.com>
2021-01-17 15:06:06 +0800

    328. Creating a Basic Chat Bubble Widget

---
+++ lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MessageBobble extends StatelessWidget {
+  MessageBobble(this.message);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).accentColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 140,
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
+          margin: EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 8,
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: Theme.of(context).accentTextTheme.title.color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
